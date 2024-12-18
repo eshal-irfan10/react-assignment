c02532573dd3d568a63896acb3dbddde31223ccc
eshal-irfan10 <developereshal8@gmail.com>
2024-12-18 07:09:21 -0800

    finaldapp

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Body from './components/Body';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
